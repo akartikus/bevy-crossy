629eaf6da70204bc34e43901bb7c54542be691ac
akartikus <sitraka.razafimamy@gmail.com>
2024-03-28 00:38:51 +0100

    Add movement

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
 struct Player;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, setup_player)
+        .add_systems(Update, character_movement)
         .run();
 }
 
-fn setup(
+fn setup_player(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
@@ -26,13 +28,37 @@ fn setup(
     let sprite_index = 24;
 
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((SpriteSheetBundle {
-        texture,
-        atlas: TextureAtlas {
-            layout: texture_atlas_layout,
-            index: sprite_index,
-        },
-        transform: Transform::from_scale(Vec3::splat(5.0)),
-        ..default()
-    },));
+    commands
+        .spawn(SpriteSheetBundle {
+            texture,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: sprite_index,
+            },
+            transform: Transform::from_scale(Vec3::splat(5.0)),
+            ..default()
+        })
+        .insert(Player);
+}
+
+fn character_movement(
+    mut characters: Query<&mut Transform, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    for mut transform in &mut characters {
+        let movement_amount = 100.0 * time.delta_seconds();
+        if input.pressed(KeyCode::KeyW) {
+            transform.translation.y += movement_amount;
+        }
+        if input.pressed(KeyCode::KeyS) {
+            transform.translation.y -= movement_amount;
+        }
+        if input.pressed(KeyCode::KeyD) {
+            transform.translation.x += movement_amount;
+        }
+        if input.pressed(KeyCode::KeyA) {
+            transform.translation.x -= movement_amount;
+        }
+    }
 }
